d8a0f7cda1077cd09656b682bb2a4fa4e5725b72
Weerawat Pleanchum <TomMacbookpro@macbook-pro-khxng-weerawat.local>
2018-09-29 17:17:48 +0700

    Initial Commit

---
+++ ExampleWithAlamo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleWithAlamo
+//
+//  Created by Weerawat Pleanchum on 29/9/2561 BE.
+//  Copyright © 2561 Tom Weerawat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
